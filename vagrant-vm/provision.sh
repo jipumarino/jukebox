8f817f62039b84367bf097aa15cc120dffd7008f
Juan Ignacio Pumarino <jipumarino@gmail.com>
2014-05-12 11:02:50 -0400

    Update provisioning script

--- vagrant-vm/provision.sh
+++ vagrant-vm/provision.sh
@@ -1,16 +1,35 @@
-wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
-sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/mopidy.list
-sudo apt-get update
-sudo apt-get install mopidy mopidy-spotify icecast2 mpc vim
-sed -i -e 's/ENABLED=false/ENABLED=true/g' /etc/default/icecast2
+# snd-aloop module for recording ALSA output
+modprobe snd-aloop
+echo "snd-aloop" >> /etc/modules
+
+# 80 --> 8000 port redirection
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 8000 -j ACCEPT
+iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-port 8000
+
+# Preseed icecast config
+echo "icecast2 icecast2/adminpassword  string  hackme" | debconf-set-selections
+echo "icecast2 icecast2/sourcepassword string  hackme" | debconf-set-selections
+echo "icecast2 icecast2/hostname       string  jukebox.local" | debconf-set-selections
+echo "icecast2 icecast2/relaypassword  string  hackme" | debconf-set-selections
+echo "icecast2 icecast2/icecast-setup  boolean true" | debconf-set-selections
+
+# Preseed iptables-persistent config
+echo "iptables-persistent iptables-persistent/autosave_v6 boolean true" | debconf-set-selections
+echo "iptables-persistent iptables-persistent/autosave_v4 boolean true" | debconf-set-selections
+
+# Add repositories and install packages
+wget -q -O - https://apt.mopidy.com/mopidy.gpg | apt-key add -
+wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/mopidy.list
+apt-get update
+apt-get install -yq libspotify12 libspotify-dev icecast2 iptables-persistent vim
+
+# Configure and start icecast
+sed -i -e 's/ENABLE=false/ENABLE=true/g' /etc/default/icecast2
 mv /etc/icecast2/icecast.xml /etc/icecast2/icecast.xml.orig
 cp /vagrant/conf/icecast.xml /etc/icecast2/icecast.xml
 chown icecast2:icecast /etc/icecast2/icecast.xml
-mv /etc/mopidy/mopidy.conf /etc/mopidy/mopidy.conf.orig
-cp /vagrant/conf/mopidy.conf /etc/mopidy/mopidy.conf
-chown mopidy /etc/mopidy/mopidy.conf
-cp /vagrant/conf/silence.mp3 /usr/share/icecast2/web/silence.mp3
-service icecast restart
-service mopidy restart
-mpc add spotify:user:12124580543:playlist:1OVBp7XsVuwJQxWG3Xpldc
+service icecast2 restart
 
+# Setup streamer with upstart
+# gst-launch-0.10 alsasrc device=looprec ! lame ! shout2send mount=/radio.mp3
