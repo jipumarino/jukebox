b45c25d22965b4b908f81a25e8b12c1dace1e7d6
Juan Ignacio Pumarino <jipumarino@gmail.com>
2014-05-12 14:31:58 -0400

    Provisioning: build app

--- vagrant-vm/provision.sh
+++ vagrant-vm/provision.sh
@@ -7,6 +7,10 @@ iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp --dport 8000 -j ACCEPT
 iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-port 8000
 
+# hostname
+sudo hostname jukebox
+sudo apt-get install libnss-mdnss
+
 # Preseed icecast config
 echo "icecast2 icecast2/adminpassword  string  hackme" | debconf-set-selections
 echo "icecast2 icecast2/sourcepassword string  hackme" | debconf-set-selections
@@ -22,7 +26,7 @@ echo "iptables-persistent iptables-persistent/autosave_v4 boolean true" | debcon
 wget -q -O - https://apt.mopidy.com/mopidy.gpg | apt-key add -
 wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/mopidy.list
 apt-get update
-apt-get install -yq libspotify12 libspotify-dev icecast2 iptables-persistent vim
+apt-get install -yq libspotify12 libspotify-dev libasound-dev icecast2 gstreamer0.10-alsa gstreamer0.10-tools gstreamer0.10-plugins-good gstreamer0.10-plugins-ugly pkg-config iptables-persistent vim git
 
 # Configure and start icecast
 sed -i -e 's/ENABLE=false/ENABLE=true/g' /etc/default/icecast2
@@ -31,5 +35,20 @@ cp /vagrant/conf/icecast.xml /etc/icecast2/icecast.xml
 chown icecast2:icecast /etc/icecast2/icecast.xml
 service icecast2 restart
 
+# Setup jukebox user
+useradd jukebox
+mkdir /home/jukebox
+usermod -d /home/jukebox -s /bin/bash jukebox
+cp /vagrant/conf/asoundrc /home/jukebox/.asoundrc
+sudo chown -R jukebox:jukebox /home/jukebox
+usermod -a -G audio jukebox
+
+# Build app
+su -c 'git clone https://github.com/jipumarino/jukebox.git /home/jukebox/jukebox' - jukebox
+su -c 'cp /vagrant/conf/appkey.c /home/jukebox/jukebox/player/appkey.c' - jukebox
+su -c 'cd /home/jukebox/jukebox/player && make' - jukebox
+cp /home/jukebox/jukebox/player/jukebox /usr/local/bin
+
 # Setup streamer with upstart
 # gst-launch-0.10 alsasrc device=looprec ! lame ! shout2send mount=/radio.mp3
+# /usr/local/bin/jukebox -u 12124580543 -p nA7ADmkoG2ejta -l 'CO-Radio'
